74b5d3406d52d411c592d5f5a40fe7cfed105ac7
Vincent Guarnaccia <Vincent.Guarnaccia@gmail.com>
2017-06-30 22:30:22 -0700

    completely rewrote examples

--- examples.py
+++ examples.py
@@ -6,10 +6,20 @@ Example:
     $ python tui_example.py
 """
 
+import time
+
 import tui
 
 
-def coloring_demo():
+def say_levels():
+    """This is the example provided by Dimitri Merejkowsky."""
+    tui.say1("Important info")
+    tui.say2("Secondary info")
+    tui.say3("This is " + tui.colorize('red', 'red'))
+    tui.say3("this is " + tui.colorize('bold', 'bold'))
+
+
+def colors_demo():
     """Print each color and style"""
     tui.say1('We have a ton of ANSI color codes and styles such as:')
     for color in tui.COLORS:
@@ -21,9 +31,7 @@ def icon_demo():
     tui.say1('Did I forget to mention the ellipsis?', tui.ELLIPSIS)
     tui.say2('The check?', tui.CHECK)
     tui.say2('The cross?', tui.CROSS)
-    tui.say2('The block?', tui.BLOCK)
-    tui.say3(
-        'They might not appear correctly on windows sadly, but they\'re super cool on bash!')
+    tui.say3('They might not appear correctly on windows sadly, but they\'re super cool on bash!')
 
 
 def enumerations_demo():
@@ -32,49 +40,44 @@ def enumerations_demo():
     list_of_things = ["foo", "bar", "baz"]
     for i, thing in enumerate(list_of_things):
         tui.countdown(i, 3, thing)
-    print(tui.ELLIPSIS)
-    tui.CONFIG['timestamp'] = True
-    tui.say1('And you can add timestamps.')
-    list_of_more_things = ["oof", "rab", "zab", "OBOE"]
-    for i, thing in enumerate(list_of_more_things, 9):
-        tui.countdown(i, 12, thing)
-    tui.CONFIG['timestamp'] = False
-    tui.say1('No more timestamps.')
-    tui.say2('It\'s hard to read.')
+
+
+@tui.proc("Isn't it great when things work out?")
+def this_function_will_succeed():
+    """Dumby function"""
+    time.sleep(3)
+
+
+@tui.proc("But even when they don't, we stay positive")
+def but_this_fails():
+    """Dumby function"""
+    time.sleep(3)
+    raise NotImplementedError
 
 
 def input_options_demo():
     """Show the input options"""
-    with_sugar = tui.ask_bool("With sugar?", default=False)
-    fruits = ["apple", "orange", "banana"]
-    selected_fruit = tui.ask_choice("Choose a fruit", fruits)
-    tui.say2(selected_fruit, "with sugar" if with_sugar else "without sugar")
+    tui.say1('TUI is very good at asking questions')
+    tui.ask_string("What's your favorite website?", default=r'https://github.com')
+    tui.ask_choice("whose your favorite?",
+                   choices=['Doc', 'Grumpy', 'Happy', 'Sleepy', 'Bashful', 'Sneezy', 'Dopey'])
+    answer = tui.ask_bool('So, do you want to see something cool?', default=True)
+    print(tui.colorize('bold', 'GREAT!' if answer else 'Too bad.'))
 
 
 def main():
     """Run all demos"""
-    coloring_demo()
-    print()
+    say_levels()
+    colors_demo()
     icon_demo()
     enumerations_demo()
     input_options_demo()
-
-
-def example():
-    """This is the example provided by Dimitri Merejkowsky."""
-    tui.say1("Important info")
-    tui.say2("Secondary info")
-    tui.say3("This is", tui.colorize('red', 'red'))
-    tui.say3("this is", tui.colorize('bold', 'bold'))
-    list_of_things = ["foo", "bar", "baz"]
-    for j, thing in enumerate(list_of_things):
-        tui.countdown(j, len(list_of_things), thing)
-    print("\n", tui.CHECK, "all done")
-    fruits = ["apple", "orange", "banana"]
-    answer = tui.ask_choice("Choose a frtuit", fruits)
-    print("You chose:", answer)
+    this_function_will_succeed()
+    try:
+        but_this_fails()
+    except NotImplementedError:
+        tui.say1('Thanks!')
 
 
 if __name__ == '__main__':
-    example()
     main()
