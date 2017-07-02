0e6080b9c957e9a6c92c4b4db171153bab40e14e
Vincent Guarnaccia <Vincent.Guarnaccia@gmail.com>
2017-07-01 17:44:09 -0700

    add setup.py
    Now we can use pip!

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""Generic Setup script"""
+
+from setuptools import setup
+
+setup(name='easy_tui',
+      version='1.0',
+      description='Modern CLI helper functions',
+      author='Vincent Guarnaccia',
+      url='https://github.com/vguarnaccia/easy_tui',
+      packages=['tui'],
+      license='BSD')
